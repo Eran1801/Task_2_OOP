4d332c694ebfaea4bee3394a4797e0d88c920e1b
Eran1801 <eranlevy9@gmail.com>
2020-12-09 22:49:51 +0200

    game init complete

--- src/gameClient/Ex2.java
+++ src/gameClient/Ex2.java
@@ -13,19 +13,65 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Ex2 {
 
     private static MyFrame _win;
     private static Arena _ar;
+    private static game_service game;
 
     public static void main(String[] args) {
 
         int level_number = 7;
-        game_service game = Game_Server_Ex2.getServer(level_number); // you have [0,23] games
+        game = Game_Server_Ex2.getServer(level_number); // you have [0,23] games
 
         init(game);
+        game.startGame();
+        //game.chooseNextEdge(0, 13);
+        //game.move();
+        //System.out.println(game.getAgents());
 
+        while(game.isRunning()) {
+            updateGameBoard();
+            try {
+                _win.repaint();
+                Thread.sleep(1000);
+            }
+            catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+        String res = game.toString();
+
+        System.out.println(res);
+        System.exit(0);
+
+    }
+
+    private static void updateGameBoard() {
+
+        String getAgentsJson = game.getAgents();
+        List<CL_Agent> agents = Arena.getAgents(getAgentsJson, _ar.getGraph());
+        _ar.setAgents(agents);
+        String getPokemonsJson =  game.getPokemons();
+        List<CL_Pokemon> pokemons = Arena.json2Pokemons(getPokemonsJson);
+        _ar.setPokemons(pokemons);
+        boolean needToMove = false;
+        for(int i=0;i<agents.size();i++) {
+            CL_Agent agent = agents.get(i);
+            int id = agent.getID();
+            int dest = agent.getNextNode();
+            int src = agent.getSrcNode();
+            double v = agent.getValue();
+            if(dest==-1) { //TODO: we stopped here, dest is always != -1 for some reason. need to check why!
+                needToMove = true;
+                dest = 13; //TODO: calculate what the next dest should be
+                game.chooseNextEdge(agent.getID(), dest);
+                System.out.println("Agent: "+id+", val: "+v+"   turned to node: "+dest);
+            }
+        }
+        if (needToMove) game.move();
     }
 
     private static void init(game_service game) {
